a37cbf0ff17221ba160ab7dcad1a8f3af4972d7c
Alexey <alexey.lobankov@bellerage.ru>
2020-12-07 02:47:04 +0300

    LoginVC keyboard handling is done

---
+++ GeekbrainsVK/Helpers/Colors.swift
@@ -0,0 +1,19 @@
+//
+//  Colors.swift
+//  GeekbrainsVK
+//
+//  Created by Alexey Lobankov on 07.12.2020.
+//
+
+import UIKit
+
+enum AppColors {
+    
+    static let lightGray   = UIColor(white: 0.95, alpha: 1)
+    
+    static let border      = UIColor(white: 0.85, alpha: 1)
+    
+    static let placeholder = UIColor(white: 0.6, alpha: 1)
+    
+    static let blue        = UIColor(red: 76/255, green: 117/255, blue: 163/255, alpha: 1)
+}
--- GeekbrainsVK/Login/LoginVC.swift
+++ GeekbrainsVK/Login/LoginVC.swift
@@ -9,12 +9,24 @@ import UIKit
 
 class LoginVC: UIViewController {
     
+    // MARK: Private fields
+    
+    private var keyboardIsVisible: Bool = false
+    
     // MARK: UI Components
     
-    private var stackView: UIStackView!
+    private var formStackView: UIStackView!
+    private var inputsStackView: UIStackView!
     private var loginInput: UITextField!
     private var passwordInput: UITextField!
     private var loginButton: UIButton!
+    
+    // MARK: Constraints
+    
+    /// Constraint that controls the Y-axis position of form stack view
+    /// - Note: Nice and long name :D
+    /// - Remark: We will tweak it's constant value in order to animate Y-axis of formStackView
+    private var formStackViewCenterYConstraint: NSLayoutConstraint!
     
     
     // MARK: Lifecycle
@@ -23,20 +35,39 @@ class LoginVC: UIViewController {
         view = UIView()
         view.backgroundColor = .white
         
-        stackView = UIStackView()
+        formStackView = UIStackView()
+        inputsStackView = UIStackView()
         loginInput = UITextField()
         passwordInput = UITextField()
         loginButton = UIButton()
         
-        view.addSubview(stackView)
-        stackView.addArrangedSubview(loginInput)
-        stackView.addArrangedSubview(passwordInput)
-        stackView.addArrangedSubview(loginButton)
+        view.addSubview(formStackView)
         
-        setupStackView()
+        formStackView.addArrangedSubview(inputsStackView)
+        
+        let separatorView = UIView()
+        separatorView.translatesAutoresizingMaskIntoConstraints = false
+        separatorView.backgroundColor = AppColors.border
+        separatorView.heightAnchor.constraint(equalToConstant: 1).isActive = true
+        
+        inputsStackView.addArrangedSubview(loginInput)
+        inputsStackView.addArrangedSubview(separatorView)
+        inputsStackView.addArrangedSubview(passwordInput)
+        
+        formStackView.addArrangedSubview(loginButton)
+        
+        setupFormStackView()
+        setupInputsStackView()
         setupLoginInput()
         setupPasswordInput()
         setupLoginButton()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        // subscribe to keyboard show and hide events...
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(notification:)), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(notification:)), name: UIResponder.keyboardWillHideNotification, object: nil)
     }
 
     override func viewDidLoad() {
@@ -44,6 +75,12 @@ class LoginVC: UIViewController {
         
         let tapGesture = UITapGestureRecognizer(target: self, action: #selector(hideKeyboard))
         view.addGestureRecognizer(tapGesture)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        // unsubscribe from keyboard events, we no longer need them
+        NotificationCenter.default.removeObserver(self)
     }
     
     // MARK: Control handlers
@@ -58,39 +95,150 @@ class LoginVC: UIViewController {
         view.endEditing(true)
     }
     
+    // MARK: Keyboard Event Handlers
+    
+    @objc private func keyboardWillShow(notification: Notification) {
+        
+        guard !keyboardIsVisible else {
+            return
+        }
+        
+        keyboardIsVisible.toggle()
+        
+        animateFormStackViewYConstraint(from: notification, show: true)
+    }
+    
+    @objc private func keyboardWillHide(notification: Notification) {
+        
+        guard keyboardIsVisible else {
+            return
+        }
+        
+        keyboardIsVisible.toggle()
+        
+        animateFormStackViewYConstraint(from: notification, show: false)
+    }
+    
+    private func animateFormStackViewYConstraint(from notification: Notification, show: Bool) {
+        
+        if let keyboardFrame = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue,
+           let duration = notification.userInfo?[UIResponder.keyboardAnimationDurationUserInfoKey] as? Double,
+           let curveValue = notification.userInfo?[UIResponder.keyboardAnimationCurveUserInfoKey] as? Int {
+            
+            // create animator object with duration and curve that exactly match iOS keyboard show / hide animation. It will look great :)
+            let animator = UIViewPropertyAnimator(duration: duration, curve: UIView.AnimationCurve(rawValue: curveValue)!, animations: nil)
+            
+            // if keyboard will show...
+            if show {
+                
+                // get topmost view
+                let rootView = UIApplication.shared.connectedScenes
+                                        .filter({ $0.activationState == .foregroundActive })
+                                        .map { $0 as? UIWindowScene }
+                                        .compactMap { $0 }
+                                        .first?.windows
+                                        .filter { $0.isKeyWindow }
+                                        .first?
+                                        .rootViewController?.view
+                
+                // get keyboard frame in the root view
+                let keyFrame = rootView!.convert(keyboardFrame.cgRectValue, to: rootView)
+                // get formStackView frame in the root view
+                let formFrame = formStackView.superview?.convert(formStackView.frame, to: rootView)
+                
+                // if these frames intersect each other...
+                if keyFrame.intersects(formFrame!) {
+                    // some nice vertical margin
+                    let extraMargin: CGFloat = 20
+                    // calculate exact offset that we need to use, in order to visibly see the login button just above the keyboard frame (with margin ofc)
+                    let offset = abs(formFrame!.maxY - keyFrame.minY) + extraMargin
+                    
+                    // add animation block
+                    animator.addAnimations { [weak self] in
+                        self?.formStackViewCenterYConstraint.constant = -offset
+                        self?.view.layoutIfNeeded()
+                    }
+                    // execute animation
+                    animator.startAnimation()
+                }
+            }
+            // if keyboard will hide...
+            else {
+                // we simply need to revert Y constraint constant back to zero
+                animator.addAnimations { [weak self] in
+                    self?.formStackViewCenterYConstraint.constant = 0
+                    self?.view.layoutIfNeeded()
+                }
+                // execute animation block
+                animator.startAnimation()
+            }
+        }
+    }
+    
     // MARK: Layout
     
-    private func setupStackView() {
+    private func setupFormStackView() {
         
-        stackView.translatesAutoresizingMaskIntoConstraints = false
+        formStackView.translatesAutoresizingMaskIntoConstraints = false
         
-        stackView.axis = .vertical
-        stackView.spacing = 5
-        stackView.distribution = .fillEqually
-        stackView.alignment = .fill
+        formStackView.axis = .vertical
+        formStackView.spacing = 5
+        formStackView.distribution = .fill
+        formStackView.alignment = .fill
+        
+        formStackView.setCustomSpacing(15, after: inputsStackView)
+        
+        // leading and trailing constraints must have less priority than width constraint.
+        // this kind of a setup will force stackView to have 20 points padding from left and right in portrait mode,
+        // but keep maximum width of 500 point in landscape, so it won't look ugly after rotating the phone :)
+        
+        let leadingConstraint = formStackView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20)
+        leadingConstraint.priority = UILayoutPriority(rawValue: 998)
+        
+        let trailingConstraint = formStackView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20)
+        trailingConstraint.priority = UILayoutPriority(rawValue: 998)
+        
+        let widthConstraint = formStackView.widthAnchor.constraint(lessThanOrEqualToConstant: 500)
+        widthConstraint.priority = UILayoutPriority(rawValue: 999)
+        
+        formStackViewCenterYConstraint = formStackView.centerYAnchor.constraint(equalTo: view.centerYAnchor)
         
         NSLayoutConstraint.activate([
-            stackView.widthAnchor.constraint(equalTo: view.widthAnchor, constant: -40),
-            stackView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            stackView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            leadingConstraint,
+            trailingConstraint,
+            widthConstraint,
+            formStackView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            formStackViewCenterYConstraint
         ])
         
     }
     
+    private func setupInputsStackView() {
+        
+        inputsStackView.translatesAutoresizingMaskIntoConstraints = false
+        
+        inputsStackView.axis = .vertical
+        inputsStackView.distribution = .fill
+        inputsStackView.alignment = .fill
+        
+        inputsStackView.clipsToBounds = true
+        
+        inputsStackView.layer.borderWidth = 1
+        inputsStackView.layer.borderColor = UIColor(white: 0.85, alpha: 1).cgColor
+        inputsStackView.layer.cornerRadius = 10
+    }
+    
     private func setupLoginInput() {
         
-        loginInput.placeholder = "Email или телефон"
         loginInput.autocapitalizationType = .none
         loginInput.font = .systemFont(ofSize: 17)
-        loginInput.textColor = .darkGray
-        loginInput.backgroundColor = UIColor(white: 0.95, alpha: 1)
-        loginInput.layer.borderWidth = 1
-        loginInput.layer.borderColor = UIColor.lightGray.cgColor
-        loginInput.layer.cornerRadius = 5
+        loginInput.textColor = .black
+        loginInput.backgroundColor = AppColors.lightGray
+        loginInput.attributedPlaceholder = NSAttributedString(string: "Email или телефон", attributes: [NSAttributedString.Key.foregroundColor: AppColors.placeholder])
         
         let userIconView = UIImageView(image: UIImage(systemName: "person"))
         userIconView.contentMode = .scaleAspectFit
-        userIconView.tintColor = .lightGray
+        userIconView.tintColor = AppColors.placeholder
         userIconView.translatesAutoresizingMaskIntoConstraints = false
         userIconView.widthAnchor.constraint(equalToConstant: 30).isActive = true
         
@@ -106,19 +254,16 @@ class LoginVC: UIViewController {
     
     private func setupPasswordInput() {
         
-        passwordInput.placeholder = "Пароль"
         passwordInput.isSecureTextEntry = true
         passwordInput.autocapitalizationType = .none
         passwordInput.font = .systemFont(ofSize: 17)
-        passwordInput.textColor = .darkGray
-        passwordInput.backgroundColor = UIColor(white: 0.95, alpha: 1)
-        passwordInput.layer.borderWidth = 1
-        passwordInput.layer.borderColor = UIColor.lightGray.cgColor
-        passwordInput.layer.cornerRadius = 5
+        passwordInput.textColor = .black
+        passwordInput.backgroundColor = AppColors.lightGray
+        passwordInput.attributedPlaceholder = NSAttributedString(string: "Пароль", attributes: [NSAttributedString.Key.foregroundColor: AppColors.placeholder])
         
         let lockIconView = UIImageView(image: UIImage(systemName: "lock"))
         lockIconView.contentMode = .scaleAspectFit
-        lockIconView.tintColor = .lightGray
+        lockIconView.tintColor = AppColors.placeholder
         lockIconView.translatesAutoresizingMaskIntoConstraints = false
         lockIconView.widthAnchor.constraint(equalToConstant: 30).isActive = true
         
@@ -134,9 +279,9 @@ class LoginVC: UIViewController {
 
     private func setupLoginButton() {
         
-        loginButton.layer.cornerRadius = 5
+        loginButton.layer.cornerRadius = 10
         
-        let normalColor = UIColor(red: 76/255, green: 117/255, blue: 163/255, alpha: 1)
+        let normalColor = AppColors.blue
         let highlightedColor = normalColor.withAlphaComponent(0.8)
         
         loginButton.setBackgroundColor(normalColor, for: .normal)
