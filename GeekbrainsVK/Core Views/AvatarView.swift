a7216254460bb68e79ce968c1628ab882ba773a3
Alexey <alexey.lobankov@bellerage.ru>
2020-12-23 09:27:59 +0300

    LikeControl added

--- GeekbrainsVK/Core Views/AvatarView.swift
+++ GeekbrainsVK/Core Views/AvatarView.swift
@@ -64,7 +64,10 @@ final class AvatarView: UIView {
     }
     
     required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
+        
+        super.init(coder: coder)
+        
+        commonInit()
     }
     
     private func commonInit() {
--- GeekbrainsVK/Friends/Views/UserPhotoCollectionViewCell.swift
+++ GeekbrainsVK/Friends/Views/UserPhotoCollectionViewCell.swift
@@ -13,15 +13,20 @@ class UserPhotoCollectionViewCell: UICollectionViewCell, Reusable {
     
     private var imageView: UIImageView!
     
+    private var likeControl: LikeControl!
+    
     // MARK: Initialization
     
     override init(frame: CGRect) {
         super.init(frame: frame)
         
         imageView = UIImageView()
+        likeControl = LikeControl()
         
+        contentView.addSubview(likeControl)
         contentView.addSubview(imageView)
         
+        setupLikeControl()
         setupImageView()
     }
     
@@ -37,11 +42,29 @@ class UserPhotoCollectionViewCell: UICollectionViewCell, Reusable {
     
     // MARK: Layout
     
+    private func setupLikeControl() {
+        
+        likeControl.setLikes(UInt.random(in: 0..<150)) 
+        
+        likeControl.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            likeControl.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
+            likeControl.widthAnchor.constraint(equalToConstant: 100),
+            likeControl.heightAnchor.constraint(equalToConstant: 30),
+            likeControl.bottomAnchor.constraint(equalTo: contentView.bottomAnchor)
+        ])
+    }
+    
     private func setupImageView() {
         
         imageView.translatesAutoresizingMaskIntoConstraints = false
         
-        imageView.fill(contentView)
+        NSLayoutConstraint.activate([
+            imageView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
+            imageView.topAnchor.constraint(equalTo: contentView.topAnchor),
+            imageView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
+            imageView.bottomAnchor.constraint(equalTo: likeControl.topAnchor, constant: -5)
+        ])
     }
-    
 }
